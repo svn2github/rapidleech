179f9ab9ac126495150fb777d311b9a1a53c4451
Hedel822@gmail.com <Hedel822@gmail.com@0f90527e-9506-11de-8d2f-ebe662690e28>
2015-04-09 17:45:24 +0000

    This is the last SVN commit of rapidleech, now it will be updated on github.
    [FIX] GetCookies and GetCookiesArr weren't reading cookies from lowercase headers. (!)
    cURL doesn't change proxy on a active resource.
    Switched geturl, cURL, upfile and putfile to TLS for https to avoid SSL related connection errors.
    Added default filename on downloads ending on a slash.
    [CHANGE] upfile now uses HTTP/1.1 (Fixes 'Origin:' header error from r430)
    getfile, upfile and putfile now uses and prefer internal dechunk filter. (Chunked files can be downloaded now)
    Rewritten insert_timer() function.
    Removed extra unused attribute from html_error().
    Added global variable for make html_error() throw the error message as an Exception.
    Added functions for the "new reCAPTCHA" on DownloadClass.
    Edited CAPTCHA functions on DownloadClass.
    Corrected some typos and warnings.
    [PLUGINS] Deleted some non existing/working hosts.
    Added some updated plugins.
    
    git-svn-id: http://rapidleech.googlecode.com/svn/trunk@431 0f90527e-9506-11de-8d2f-ebe662690e28

--- hosts/download/180upload_com.php
+++ hosts/download/180upload_com.php
@@ -8,8 +8,8 @@ if (!defined('RAPIDLEECH')) {
 if (!file_exists(HOST_DIR . 'download/GenericXFS_DL.php')) html_error('Cannot load "'.htmlentities(HOST_DIR).'download/GenericXFS_DL.php" (File doesn\'t exists)');
 require_once(HOST_DIR . 'download/GenericXFS_DL.php');
 
-class uptobox_com extends GenericXFS_DL {
-	public $pluginVer = 2;
+class d180upload_com extends GenericXFS_DL {
+	public $pluginVer = 1;
 	public function Download($link) {
 		$this->wwwDomain = false; // Switch to true if filehost forces it's domain with www.
 		$this->cname = 'xfss'; // Session cookie name
@@ -17,11 +17,6 @@ class uptobox_com extends GenericXFS_DL {
 		$this->embedDL = false; // Try to unpack player's js for finding download link. (Only hosts with video player)
 
 		$this->Start($link);
-	}
-
-	// Patched function for UTB bad https login.
-	protected function sendLogin($post) {
-		return $this->GetPage((!empty($this->sslLogin) ? 'https://'.$this->host.'/' : $this->purl) . '?op=login', $this->cookie, $post, 'https://login.uptobox.com/');
 	}
 }
 
